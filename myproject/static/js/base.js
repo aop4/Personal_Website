cf78a7e2cdcd0f44cb5c2c7b206fb2bb61d19398
Andrew Puglionesi <aop4@pitt.edu>
2018-05-10 22:21:43 -0400

    Add forest theme

--- myproject/static/js/base.js
+++ myproject/static/js/base.js
@@ -1,19 +1,27 @@
 $(document).ready(function() {
-	$("body").css("visibility", "visible");
+	
 	var activeNavbarItem = $("#navbar-activation-tag").val();
 	$("#"+activeNavbarItem).addClass("active");
 	setIconVisibility(activeNavbarItem);
-
+	setBodyClass(activeNavbarItem);
 	$("#lang-picker").on('mouseover', function() {
 		$(this).click(); //expand the dropdown on mouseover
 	});
 	$("#lang-choices").on('mouseleave', function() {
 		$("#lang-picker").click(); //collapse the dropdown on mouseout
 	});
+	$("body").css("visibility", "visible");
 });
 
 function setIconVisibility(activeNavbarItem) {
 	if (activeNavbarItem == "photography" || activeNavbarItem == "web" || activeNavbarItem == "home") {
 		$(".web-icon-bar").hide("slow"); //parameter needed to hide icons in chrome because DOM not ready
+	}
+}
+
+function setBodyClass(activeNavbarItem) {
+	//add in the forest background where desired
+	if (activeNavbarItem != "photography") {
+		$("body").addClass("woods");
 	}
 }
