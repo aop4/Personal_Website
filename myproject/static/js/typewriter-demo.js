1927f7675020c8f1b1e28de0569a766c77700e27
Andrew Puglionesi <aop4@pitt.edu>
2018-06-05 21:41:54 -0400

    Update demo

--- myproject/static/js/typewriter-demo.js
+++ myproject/static/js/typewriter-demo.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
 
 	var titleWriter = new NaturalTypewriter({'interval':50,
 		'flexibility':40});
-	titleWriter.write(document.getElementById('header'), 'naturaltypewriter.js');
-	titleWriter.write(document.getElementById('natural'),
-		"Wondering what's so \"natural\" about it?");
+	titleWriter.write({'domElement':document.getElementById('header'), 
+		'text':'naturaltypewriter.js'});
+	titleWriter.write({'domElement':document.getElementById('natural'),
+		'text':"Wondering what's so \"natural\" about it?", 'delay':700});
 
 	var flexConfig = {'interval':80, 'flexibility':40};
 
@@ -22,34 +23,34 @@ $(document).ready(function() {
 		'flexibility':40, 'infinite':true, 'audioSource':''});
 	var lastWriter = new NaturalTypewriter({'interval':100});
 
-	setTimeout(writeToVanilla, 4000);
+	setTimeout(writeToVanilla, 5000);
 
 	function writeToVanilla() {
-		vanillaWriter.write(document.getElementById('vanilla'), 
-			'Well, you can make it look mechanical by having it type '+
+		vanillaWriter.write({'domElement':document.getElementById('vanilla'), 
+			'text':'Well, you can make it look mechanical by having it type '+
 			'characters at a constant speed. But go ahead and '+
-			'hit the down button...');
+			'hit the down button...'});
 	}
 
 	function writeToFlexible() {
-		flexWriter.write(document.getElementById('flexible'),
-			"You can also make the speed vary from "+
+		flexWriter.write({'domElement':document.getElementById('flexible'),
+			'text':"You can also make the speed vary from "+
 			"one character to the next, since the way people actually "+
-			"type isn't quite the way a robot would type.");
+			"type isn't quite the way a robot would type."});
 	}
 
 	function writeToBacktrack() {
-		backTrackWriter.write(document.getElementById('backtrack'),
-			"There's also support for backtracking--when you mess up a "+
-			"character, realize your mistake, and replace it.");
+		backTrackWriter.write({'domElement':document.getElementById('backtrack'),
+			'text':"There's also support for backtracking--when you mess up a "+
+			"character, realize your mistake, and replace it."});
 	}
 
 	function writeToChance() {
-		chanceWriter.write(document.getElementById('chance'),
-			"Some libraries force you to plan out changes in the flow of typing in "+
+		chanceWriter.write({'domElement':document.getElementById('chance'),
+			'text':"Some libraries force you to plan out changes in the flow of typing in "+
 			"painstaking detail, but with naturaltypewriter.js, it's easy. "+
 			"You tell it the basics of how you want it to act, and the precise details " +
-			"are left up to chance.");
+			"are left up to chance."});
 	}
 
 	function writeToEnd() {
@@ -58,9 +59,9 @@ $(document).ready(function() {
 		$('#end-3').text('');
 		//you'll note this function uses two ways to get an element by its id... I'm
 		//I'm following the style from my docs, which favor nativs JS, where applicable
-		lastWriter.write(document.getElementById('end-1'), "Your fonts...");
-		lastWriter.append(document.getElementById('end-2'), "\nYour story...");
-		lastWriter.append(document.getElementById('end-3'), "\nJust a bit more captivating.");
+		lastWriter.write({'domElement':document.getElementById('end-1'), 'text':"Your fonts..."});
+		lastWriter.append({'domElement':document.getElementById('end-2'), 'text':"\nYour story..."});
+		lastWriter.append({'domElement':document.getElementById('end-3'), 'text':"\nJust a bit more captivating."});
 	}
 
 	var typingFunctions = {
