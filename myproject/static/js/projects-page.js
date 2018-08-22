2096a5266b6f506a88d9d427263848cb3dc67b52
Andrew Puglionesi <aop4@pitt.edu>
2018-08-22 10:49:50 -0400

    Typewriter placeholder in Spanish

--- myproject/static/js/projects-page.js
+++ myproject/static/js/projects-page.js
@@ -27,13 +27,18 @@ $(document).ready(function() {
 		typewriterDescription.innerHTML = '';
 		$('#typewriter-demo').text('');
 		//write out the slide's contents in order
-		appendText($('#typewriter-repo')[0], 'NaturalTypewriter.js', 0);
-		appendText(typewriterDescription, " is a native JavaScript typewriter. ", 0);
-		appendText(typewriterDescription, "I would explain what that means, but it's probably pretty clear by this point. ", 1000)
-		appendText(typewriterDescription, "I am kind of obsessed with random number generators, so I wanted to see what it would be like if a typewriter library operated with a random speed and random errors to simulate a person typing. ", 1000);
-		appendText(typewriterDescription, "It ended up being an interesting, thought-provoking project that took me into the depths of JavaScript and asynchronous programming and forced me to think more about library usability than I've ever had to. ", 1000);
-		appendText(typewriterDescription, "Here's the real ", 1000);
-		appendText($('#typewriter-demo')[0], 'demo.', 0);
+		if ($('#language').val() === "English") {
+			appendText($('#typewriter-repo')[0], 'NaturalTypewriter.js', 0);
+			appendText(typewriterDescription, " is a native JavaScript typewriter. ", 0);
+			appendText(typewriterDescription, "I would explain what that means, but it's probably pretty clear by this point. ", 1000)
+			appendText(typewriterDescription, "I am kind of obsessed with random number generators, so I wanted to see what it would be like if a typewriter library operated with a random speed and random errors to simulate a person typing. ", 1000);
+			appendText(typewriterDescription, "It ended up being an interesting, thought-provoking project that took me into the depths of JavaScript and asynchronous programming and forced me to think more about library usability than I've ever had to. ", 1000);
+			appendText(typewriterDescription, "Here's the real ", 1000);
+			appendText($('#typewriter-demo')[0], 'demo.', 0);
+		}
+		else {
+			appendText(typewriterDescription, "Lo siento, pero no he cumplido una traducción de esta descripción.")
+		}
 	}
 
 	function appendText(domElement, text, delay) {
