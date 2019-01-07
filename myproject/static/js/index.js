3c87b353e15dbaa5c9c217d896fd53ec264f00c1
Andrew Puglionesi <aop4@pitt.edu>
2019-01-06 19:36:30 -0500

    Add alert about site being old to home page

---
+++ myproject/static/js/index.js
@@ -0,0 +1,3 @@
+alert("You've come to my old website. It still has some content I'd like " +
+"to hold onto, so it's not going anywhere. If you'd like to see what " +
+"I've been up to more recently, head over to to www.andrewpuglionesi.com. Thanks!");
